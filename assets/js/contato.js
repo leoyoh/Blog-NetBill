226c8202dcdf72b0ac346b15f8e342f90baafaa4
leoyoh <54145859+leoyoh@users.noreply.github.com>
2025-10-01 23:51:35 -0300

    Contato, posts
    - Adicionado o modal de contato em todas as paginas
    - Adicionado a pagina posts para abrir os posts
    - Removido o conteudo das posts anteriormente salvos, alterado apenas por um lorem padrao para todas ja que o intuito do teste nao é esse
    -

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,11 +1,47 @@
 // recebe os dados post do form contato e manda para api
+function modalContato() {
+    const btnAbrir = document.getElementById("abrirContato");
+    const overlay = document.getElementById("overlay");
+    const btnFechar = document.getElementById("fecharModal");
+
+    // Abrir modal
+    btnAbrir.addEventListener("click", () => {
+        overlay.style.display = "flex"; // aparece com flex (centralizado)
+    });
+
+    // Fechar modal clicando no X
+    btnFechar.addEventListener("click", () => {
+        overlay.style.display = "none";
+    });
+
+    // Fechar modal clicando fora dele (no fundo escuro)
+    overlay.addEventListener("click", (e) => {
+        if (e.target === overlay) { // só fecha se clicar no fundo
+            overlay.style.display = "none";
+        }
+    });
+}
 
 function dadosContato() {
+    //evita que a pagina recarregue
+    event.preventDefault();
+
+    //pega as info vindas dos inputs e salva em variaveis
     let nome = document.getElementById('nome').value.trim();
     let email = document.getElementById('email').value.trim();
-    let telefone = document.getElementById('contato').value.trim();
+    let telefone = document.getElementById('telefone').value.trim();
     let cidade = document.getElementById('cidade').value.trim();
     let mensagem = document.getElementById('mensagem').value.trim();
+
+    //verifica se no campo email existe pelo menos um @
+    if (!email.includes("@")) {
+        alert("Digite um e-mail válido.");
+        return;
+    }   
+
+
+
+    //post para api, disponibilizado no enunciado do teste
 
     axios.post("https://netbil.com.br/api_netbil/api-teste-programacao/contato", { nome, email, telefone, cidade, mensagem }, {
         headers: {
@@ -13,10 +49,14 @@ function dadosContato() {
         }
     }).then(({ data }) => {
         console.log(data);
-        alert("Mensagem enviada com sucesso!");
+        //reseta o formulario
         document.getElementById("form-contato").reset();
+        //exibe um alerta de que a mensagem foi enviada
+        alert("Mensagem enviada com sucesso!");
+        
     }).catch(err => {
         console.error("Erro ao enviar form", err);
+        //exibe um alerta caso a msg nao tenha sido enviada
         alert("Erro ao enviar mensagem.");
     });
 
